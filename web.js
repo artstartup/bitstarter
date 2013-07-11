54e7002393728d9bde8ee5add1eeed10dfca4844
Artemij Fedosejev <artemij.startup@gmail.com>
2013-07-11 15:19:11 +0000

    Updated web.js. Bug fix.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFile('index.html', function(err, data) {
     if (err) throw err;
-    var buf = new Buffer(256);
+    var buf = new Buffer(data, "utf-8");
     response.send(buf.toString());
   });
 });
